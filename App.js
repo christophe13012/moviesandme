db0bc46e1bca8257b4386d866adeab357b77e440
chris13012 <christophecaillet@icloud.com>
2019-08-23 16:16:21 +0200

    first

---
+++ App.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Constants from 'expo-constants';
+import Navigation from './Navigation/Navigation'
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Navigation/>
+    );
+  }
+}
+
+
---
+++ components/FilmDetail.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import { StyleSheet, View, Text, ActivityIndicator, ScrollView } from 'react-native'
+import { getFilmDetailFromApi } from '../API/TMDBApi'
+
+class FilmDetail extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      film: undefined,
+      isLoading: true
+    }
+  }
+
+  componentDidMount() {
+    getFilmDetailFromApi(this.props.navigation.state.params.idFilm).then(data => {
+      this.setState({
+        film: data,
+        isLoading: false
+      })
+    })
+  }
+
+  _displayLoading() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.loading_container}>
+          <ActivityIndicator size='large' />
+        </View>
+      )
+    }
+  }
+
+  _displayFilm() {
+    if (this.state.film != undefined) {
+      return (
+        <ScrollView style={styles.scrollview_container}>
+          <Text style={styles.title}>{this.state.film.title}</Text>
+          {/* Pour l'instant je n'affiche que le titre, je vous laisserais le soin de créer la vue. Après tout vous êtes aussi là pour ça non ? :)*/}
+        </ScrollView>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.main_container}>
+        {this._displayLoading()}
+        {this._displayFilm()}
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  main_container: {
+    flex: 1
+  },
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  scrollview_container: {
+    flex: 1
+  },
+  title : {fontSize: 24}
+})
+
+export default FilmDetail
